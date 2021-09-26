523b37bd2a8dc27ac92050ed50ca246e99a9f6e3
Justice Velasco <justicevgomez001@gmail.com>
2021-09-26 22:59:36 +0200

    album category updated

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,39 +1,23 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import * as React from 'react'
+import { StyleSheet, FlatList } from 'react-native'
 
-import { Text, View } from '../components/Themed';
-import AlbumCategory from '../components/AlbumCategory';
-
-const albumCategory = {
-  id: '1',
-  title: 'Happy Vibes',
-  albums: [
-    {
-      id: '1',
-      imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
-      artistsHeadline: 'Taylor Swift, Kygo Objective C, Avicii'
-    }, {
-      id: '2',
-      imageUri: 'https://cdn6.f-cdn.com/contestentries/1485199/27006121/5ca3e39ced7f1_thumb900.jpg',
-      artistsHeadline: 'Post Malone, Drake, Eminem'
-    },
-    {
-      id: '3',
-      imageUri: 'https://images-na.ssl-images-amazon.com/images/I/61F66QURFyL.jpg',
-      artistsHeadline: 'Journey, Escape, Avicii'
-    },
-    {
-      id: '4',
-      imageUri: 'https://i.pinimg.com/originals/a2/0d/37/a20d37791f8ad5cd54734cd3af559cc9.jpg',
-      artistsHeadline: 'Bob Marley, Cardi B, Stas Mihailov'
-    },
-  ]
-};
+import { Text, View } from '../components/Themed'
+import AlbumCategory from '../components/AlbumCategory'
+import albumCategories from '../data/albumCategories'
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <AlbumCategory title={albumCategory.title} albums={albumCategory.albums} />
+      <FlatList 
+        data={albumCategories}
+        renderItem={({ item }) => (
+          <AlbumCategory 
+            title={item.title}
+            albums={item.albums} 
+            keyExtractor={item.id}
+          /> 
+          )}
+      />
     </View>
   );
 }
