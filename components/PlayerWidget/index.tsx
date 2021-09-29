f4e239b8e507a8d7d78a2c2ec5c75ce47a50f514
Justice Velasco <justicevgomez001@gmail.com>
2021-09-29 18:29:21 +0200

    functions to play song created

--- components/PlayerWidget/index.tsx
+++ components/PlayerWidget/index.tsx
@@ -1,18 +1,53 @@
-import React from 'react'
-import { View, Image, Text  } from 'react-native'
-import { AntDesign, FontAwesome } from '@expo/vector-icons'
+import React, {useEffect, useState} from 'react'
+import { View, Image, Text, TouchableOpacity } from 'react-native'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { Audio } from 'expo-av'
 
 import styles from './styles'
 
-
 const song = {
     id: '1',
+    uri: 'https://olagist.net/wp-content/uploads/2019/09/Post_Malone_-_Circles.mp3?_=1',
     imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
     title: 'High on You',
     artist: 'Helen',
 }
 
 const PlayerWidget = () => {
+
+    const [sound, setSound] = useState<Sound|null>(null);
+    const [isPlaying, setIsPlaying] = useState<boolean>(true);
+    
+    const onPlaybackStatusUpdate = (status) => {
+        setIsPlaying(status.isPlaying);
+    }
+
+    const playCurrentSong = async () => {
+        if (sound) {
+            await sound.unloadAsync();
+        }
+        const { sound: newSound } = await Audio.Sound.createAsync(
+            { uri: song.uri },
+            { shouldPlay: isPlaying },
+            onPlaybackStatusUpdate
+        )
+        setSound(newSound)
+    }
+
+    useEffect( () => {
+        playCurrentSong();
+    }, [])
+
+    const onPlayPausePress = async () => {
+        if (!sound) {
+            return;
+        }
+        if (isPlaying) {
+            await sound.stopAsync();
+        }else{
+            await sound.playAsync();
+        }
+    }
 
     return (
         <View style={styles.container}>
@@ -27,7 +62,9 @@ const PlayerWidget = () => {
                 
                 <View style={styles.buttons}>
                     <FontAwesome name="heart-o" size={24} color="white" style={{marginRight: 10}} />
-                    <AntDesign name="pause" size={24} color="white" />
+                    <TouchableOpacity onPress={onPlayPausePress} >
+                        <Ionicons name={isPlaying ? "pause-outline" : "md-play"} size={26} color="white" />
+                    </TouchableOpacity>
                 </View>
             </View>
 
